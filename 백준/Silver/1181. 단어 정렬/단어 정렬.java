2c9026ff709487b2115efba8da1879e307d26262
Youngjun Son <84082544+sohn919@users.noreply.github.com>
2023-05-31 22:22:19 +0900

    [Silver V] Title: 단어 정렬, Time: 348 ms, Memory: 24616 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -9,17 +8,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        Set<String> str = new TreeSet<>(new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
+        Set<String> str = new TreeSet<>((o1, o2) -> {
                 if(o1.length() == o2.length()) return o1.compareTo(o2);
                 else if(o1.length() > o2.length()) return 1;
                 else return -1;
-            }
         });
         for(int i=0; i<N; i++) {
             str.add(br.readLine());
         }
-        str.forEach(System.out::println);
+        StringBuilder sb = new StringBuilder();
+        str.forEach(x -> sb.append(x).append("\n"));
+        System.out.print(sb);
     }
 }
