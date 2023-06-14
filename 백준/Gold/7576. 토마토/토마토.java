6dea492e41ab00c967d40b6d55fcf426054b7160
Youngjun Son <84082544+sohn919@users.noreply.github.com>
2023-06-15 00:39:04 +0900

    [Gold V] Title: 토마토, Time: 680 ms, Memory: 121824 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -7,72 +7,60 @@ import java.util.StringTokenizer;
 
 class Point {
     int x, y;
-    Point(int x, int y) {
+    public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 }
 
 public class Main {
-    static int N, M;
-    static int[][] board;
-    static boolean[][] isvisit;
-    static Queue<Point> q;
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
-
-    public static void BFS() {
-        while(!q.isEmpty()) {
-            Point o = q.poll();
-            for(int i=0; i<4; i++) {
-                int nx = o.x + dx[i];
-                int ny = o.y + dy[i];
-                if(nx < 0 || nx >= M || ny < 0 || ny >= N || isvisit[nx][ny] || board[nx][ny] != 0)
-                    continue;
-                q.add(new Point(nx, ny));
-                isvisit[nx][ny] = true;
-                board[nx][ny] = board[o.x][o.y] + 1;
-            }
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.valueOf(st.nextToken());
-        M = Integer.valueOf(st.nextToken());
-        board = new int[M][N];
-        isvisit = new boolean[M][N];
-        for(int i=0; i<M; i++) {
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int[][] board = new int[N][M];
+        Queue<Point> Q = new LinkedList<>();
+        for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++) {
-                board[i][j] = Integer.valueOf(st.nextToken());
+            for(int j=0; j<M; j++) {
+                int value = Integer.parseInt(st.nextToken());
+                if(value == 1) {
+                    board[i][j] = 1;
+                    Q.add(new Point(i, j));
+                } else board[i][j] = value;
             }
         }
-
-        q = new LinkedList<>();
-
-        for(int i=0; i<M; i++) {
-            for(int j=0; j<N; j++) {
-                if(board[i][j] == 1) {
-                    q.add(new Point(i, j));
-                    isvisit[i][j] = true;
-                }
-            }
-        }
-        BFS();
-        int answer = 0;
-        for(int i=0; i<M; i++) {
-            for(int j=0; j<N; j++) {
+        int answer = BFS(N, M, board, Q);
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
                 if(board[i][j] == 0) {
                     System.out.print(-1);
                     return;
                 }
-                answer = Math.max(answer, board[i][j]);
             }
         }
-        System.out.print(answer - 1);
+        System.out.print(answer);
+    }
 
-
+    private static int BFS(int N, int M, int[][] board, Queue<Point> Q) {
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, -1, 0, 1};
+        int cnt = 0;
+        while(!Q.isEmpty()) {
+            int size = Q.size();
+            while(size-- > 0) {
+                Point cur = Q.poll();
+                for (int i=0; i<4; i++) {
+                    int nx = cur.x + dx[i];
+                    int ny = cur.y + dy[i];
+                    if (nx < 0 || nx >= N || ny < 0 || ny >= M || board[nx][ny] != 0) continue;
+                    board[nx][ny] = 1;
+                    Q.add(new Point(nx, ny));
+                }
+            }
+            cnt++;
+        }
+        return cnt-1;
     }
 }
