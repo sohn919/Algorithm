4698b8af5e950c0bbf924b8bb6edb8aae92ec391
Youngjun Son <84082544+sohn919@users.noreply.github.com>
2023-10-19 19:02:32 +0900

    [level 3] Title: 최고의 집합, Time: 0.02 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int n, int s) {
+        if(n > s) return new int[] {-1};
+        int[] answer = new int[n];
+        int idx = 0;
+        while(n > 0){
+            int value = s/n;
+            answer[idx++] = value;
+            s -= value;
+            n--;
+        }
+        return answer;
+    }
+}
